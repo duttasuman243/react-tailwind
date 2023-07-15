521ec01ecb1c46d8388c41d520637b3275e2c306
duttasuman243 <duttasuman243@gmail.com>
2023-07-15 12:05:53 +0530

    upadte

--- src/components/about/AboutCounter.jsx
+++ src/components/about/AboutCounter.jsx
@@ -1,41 +1,88 @@
-import { useCountUp } from 'react-countup';
-import CounterItem from './CounterItem';
-
 const AboutCounter = () => {
-	useCountUp({ ref: 'experienceCounter', end: 12, duration: 2 });
-	useCountUp({ ref: 'githubStarsCounter', end: 20, duration: 2 });
-	useCountUp({ ref: 'feedbackCounter', end: 92, duration: 2 });
-	useCountUp({ ref: 'projectsCounter', end: 77, duration: 2 });
-
 	return (
-		<div className="mt-10 sm:mt-20 bg-primary-light dark:bg-ternary-dark shadow-sm">
-			<div className="font-general-medium container mx-auto py-20 block sm:flex sm:justify-between items-center">
-				<CounterItem
-					title="Years of experience"
-					counter={<span id="experienceCounter" />}
-					measurement=""
-				/>
-
-				<CounterItem
-					title="Stars on GitHub"
-					counter={<span id="githubStarsCounter" />}
-					measurement="k+"
-				/>
-
-				<CounterItem
-					title="Positive feedback"
-					counter={<span id="feedbackCounter" />}
-					measurement="%"
-				/>
-
-				<CounterItem
-					title="Projects completed"
-					counter={<span id="projectsCounter" />}
-					measurement="%"
-				/>
-			</div>
+	  <div className="mt-10 sm:mt-20 bg-primary-light dark:bg-ternary-dark shadow-sm">
+		<div style={{
+			margin:'auto',display: 'block'
+		}} className="font-general-medium container mx-auto  block sm:flex sm:justify-between items-center">
+		  <div className="education-container">
+			<cemter><h2 className="font-general-medium text-2xl sm:text-3xl  text-center text-primary-dark dark:text-primary-light" style={{
+				textAlign:'center',marginBottom:'20px',paddingTop:20
+			}}>Education</h2></cemter>
+			<ul className="education-list">
+			  <li>
+				<div>
+				  <strong>Calcutta Institute of Engineering and Management, Kolkata</strong>
+				  <br />
+				  B.Tech in Computer Science Engineering
+				</div>
+				<div>
+				  <span>2018-2022</span>
+				  <br />
+				  <span>CGPA: 8.58 (out of 10)</span>
+				</div>
+			  </li>
+			  <li>
+				<div>
+				  <strong>Howrah Vivekananda Institution, Howrah</strong>
+				  <br />
+				  Higher Secondary
+				</div>
+				<div>
+				  <span>2018</span>
+				  <br />
+				  {/* <span>Percentage: 52.33</span> */}
+				</div>
+			  </li>
+			  <li>
+				<div>
+				  <strong>Howrah Vivekananda Institution, Howrah</strong>
+				  <br />
+				  Secondary
+				</div>
+				<div>
+				  <span>2015</span>
+				  <br />
+				  {/* <span>Percentage: 69.14</span> */}
+				</div>
+			  </li>
+			</ul>
+		  </div>
 		</div>
+		<style jsx>{`
+		  .education-container {
+			margin-top: 2rem;
+		  }
+		  
+		  .education-list {
+			list-style-type: none;
+			padding: 0;
+			margin: 0;
+			display: flex;
+		width:100%;  
+		flex-wrap:wrap;
+		gap:20px;
+		}
+		  
+		  .education-list li {
+			margin-bottom: 1.5rem;
+			flex:1;
+		  }
+		  
+		  .education-list div {
+			margin-bottom: 0.5rem;
+		  }
+		  
+		  .education-list strong {
+			font-weight: bold;
+		  }
+		  
+		  .education-list span {
+			display: block;
+		  }
+		`}</style>
+	  </div>
 	);
-};
-
-export default AboutCounter;
+  };
+  
+  export default AboutCounter;
+  
--- src/data/clientsData.js
+++ src/data/clientsData.js
@@ -1,54 +1,38 @@
 // Import images
-import AmazonImage from '../images/brands/amazon_gray.png';
-import SonyImage from '../images/brands/sony_gray.png';
-import AdidasImage from '../images/brands/adidas_gray.png';
-import FilaImage from '../images/brands/fila_gray.png';
-import NBImage from '../images/brands/nb_gray.png';
-import SamsungImage from '../images/brands/samsung_gray.png';
-import CanonImage from '../images/brands/canon_gray.png';
-import PumaImage from '../images/brands/puma_gray.png';
 
-export const clientsHeading = 'Some of the brands that trust me';
+import Figma from '../images/figma.png'
+import Photoshop from '../images/photoshop.png'
+import Ai from '../images/ai.png'
+import Wireframing from '../images/wireframing.png'
+import Prototyping from '../images/prototyping.png'
+
+export const clientsHeading = 'Skills';
 
 export const clientsData = [
 	{
 		id: 1,
-		title: 'Amazon',
-		img: AmazonImage,
+		title: 'Figma',
+		img: Figma,
 	},
 	{
 		id: 2,
-		title: 'Sony',
-		img: SonyImage,
+		title: 'Photoshop',
+		img: Photoshop,
 	},
 	{
 		id: 3,
-		title: 'Adidas',
-		img: AdidasImage,
+		title: 'Ai',
+		img: Ai,
 	},
 	{
 		id: 4,
-		title: 'FILA',
-		img: FilaImage,
+		title: 'Wireframing',
+		img: Wireframing,
 	},
 	{
 		id: 5,
-		title: 'NB',
-		img: NBImage,
+		title: 'Prototyping',
+		img: Prototyping,
 	},
-	{
-		id: 6,
-		title: 'SAMSUNG',
-		img: SamsungImage,
-	},
-	{
-		id: 7,
-		title: 'CANON',
-		img: CanonImage,
-	},
-	{
-		id: 8,
-		title: 'PUMA',
-		img: PumaImage,
-	},
+	
 ];
