6b8ffdbb6b50f64afed6c30e5aefa9d2dd8c8cb6
duttasuman243 <duttasuman243@gmail.com>
2023-07-15 12:20:43 +0530

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,12 +5,13 @@ import WebImage2 from '../images/LandingPage1+cover.png';
 import MobileImage2 from '../images/loginpage2+cover.png';
 import UIImage1 from '../images/luggage4cover.png';
 import UIImage2 from '../images/medicongrp1+cover.png';
+import Municipal from '../images/municipal.png'
 
 export const projectsData = [
 	{
 		id: 1,
 		title: 'E-commerce Landing Page',
-		category: 'Web Application',
+		category: 'Introducing sleek and user-friendly Landing Page',
 		img: WebImage2,
 		link:"https://www.figma.com/file/HD4em7ZWx1RLpc5DsPGETw/Landing-page?type=design&node-id=0-1&mode=design&t=JGRNmEB43dr5lfn9-0",
 		ProjectHeader: {
@@ -23,22 +24,29 @@ export const projectsData = [
 	{
 		id: 2,
 		title: 'Login Pages',
-		category: 'Mobile Application',
+		category: 'Access your account effortlessly and embark on your digital journey with ease',
 		img: MobileImage2,
 		link:"https://www.figma.com/file/85isxDF5BSRxUqYVeLAURt/Login-page?type=design&node-id=0-1&mode=design&t=WNPePJWNM9y6bVp9-0",
 	},
 	{
 		id: 3,
 		title: 'Luggage E-commerce Website',
-		category: 'UI/UX Design',
+		category: 'Seamless travel experience with sleek and intuitive UI/UX design',
 		img: UIImage1,
 		link:"https://www.figma.com/file/7o7IhNifzlkSwuleX7glB9/BuyLuggageOnline?type=design&node-id=0-1&mode=design&t=r9fSgem4DtqW6nvh-0"
 	},
 	{
 		id: 4,
 		title: 'Medicon Group Website',
-		category: 'UI/UX Design',
+		category: 'Revolutionizing the digital experience for medical care',
 		img: UIImage2,
 		link:"https://www.figma.com/file/j4CzHM95mZzj5vdkKbyuYn/MEDICON-GROUP?type=design&node-id=0-1&mode=design&t=UpF5EnPLltQ7FtNc-0"
+	},
+	{
+		id: 5,
+		title: 'Re-design of Municipality Website',
+		category: 'Unleashing a new design that captivates users & enhances functionality',
+		img: Municipal,
+		link:"https://www.figma.com/file/E5alA9GnlJbv4hlXhdEu2W/Municipal-Work?type=design&node-id=0-1&mode=design&t=Et6IY4EJU2krfW03-0"
 	},
 ];
